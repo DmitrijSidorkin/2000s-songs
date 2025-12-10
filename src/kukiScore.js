521cd7edf354fc7d22044779797b44d57191d530
Dmitrij Sidorkin <dmitrij,sidorkin95@gmail.com>
2025-12-10 14:49:02 +0200

    initial commit

---
+++ src/kukiScore.js
@@ -0,0 +1,258 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const kukiScore = {"Kryptonite":   135,
+"Don't Trust Me":   167,
+"In Da Club":   106,
+"Around The World":   60,
+"Whataya Want from Me":   88,
+"Chasing Pavements":   162,
+"Because I got high":   183,
+"Release me":   141,
+"Lonely":   115,
+"Fallin'":   128,
+"Smooth Criminal":   84,
+"1 Thing":   178,
+"Back To Black":   168,
+"I'm Outta Love":   102,
+"The Tide is High":   140,
+"Complicated":   28,
+"Incomplete":   159,
+"Where's Your Head At":   179,
+"Boten Anna":   76,
+"Crazy In Love":   39,
+"Let's Get It Started":   21,
+"I Miss You":   79,
+"Foxtrot Unicorn Charlie Kilo":   151,
+"All Rise":   24,
+"Rock This Party":   126,
+"It's My Life":   49,
+"1985":   52,
+"The Diary of Jane":   6,
+"Toxic":   132,
+"Year 3000":   63,
+"Everytime We Touch":   117,
+"I'm Alive":   197,
+"Ridin'":   40,
+"Right Thurr":   107,
+"Forever":   95,
+"Lady Marmalade":   161,
+"Dip It Low":   186,
+"1,2, Step":   145,
+"Bubbly":   160,
+"Clocks":   110,
+"Put Your Records On":   192,
+"7 Days":   169,
+"Axel F":   171,
+"One Last Breath":   105,
+"Butterfly":   96,
+"Gasolina":   137,
+"One More Time":   34,
+"If You're Not The One":   148,
+"Red":   184,
+"Bad Day":   80,
+"Pump It Up":   189,
+"Sandstorm":   72,
+"Love Don't Let Me Go":   103,
+"Survivor":   70,
+"White Flag":   74,
+"The Next Episode":   11,
+"Mercy":   172,
+"Stereo Love":   26,
+"Home":   177,
+"The Real Slim Shady":   7,
+"Hero":   146,
+"Pjanoo":   136,
+"Bring Me To Life":   18,
+"Dance, Dance":   56,
+"Be Faithful":   187,
+"Right Round":   127,
+"The Pretender":   108,
+"Take Me Out":   1,
+"I Don't Want to Be":   133,
+"Crazy":   188,
+"Feel Good Inc.":   22,
+"Boulevard Of Broken Dreams":   48,
+"Infinity ":   57,
+"Cupid's Chokehold":   182,
+"Rich Girl":   153,
+"The Reason":   134,
+"Let Me Think About It":   185,
+"I'm Yours":   43,
+"99 Problems":   64,
+"Superstar":   180,
+"You're Beautiful":   196,
+"Whatcha Say":   166,
+"Down":   59,
+"Jenny From The Block":   71,
+"Birthday Sex":   152,
+"The Middle":   27,
+"Your Body Is A Wonderland":   37,
+"Hurt":   83,
+"Leave (Get Out)":   109,
+"No Air":   122,
+"La Camisa Negra":   98,
+"Move Your Feet":   99,
+"Baby":   175,
+"Rock Your Body":   13,
+"Gold Digger":   75,
+"Nine Millions Bicycles":   198,
+"I Kissed A Girl":   113,
+"Everybody's Changing":   50,
+"Milkshake":   104,
+"Because Of You":   130,
+"Let It Rock":   154,
+"Turn Me On":   116,
+"Day 'n' Nite":   82,
+"All Summer Long":   176,
+"Use Somebody":   86,
+"Suddenly I See":   199,
+"Can't Get You Out Of My Head":   94,
+"Need You Now ":   55,
+"Just Dance":   73,
+"The Ketchup Song":   119,
+"Can't Fight The Moonlight":   31,
+"Bleeding Love":   66,
+"Hanging By A Moment":   23,
+"Get Low":   81,
+"Smile":   174,
+"Behind Blue Eyes":   101,
+"In The End":   5,
+"You":   112,
+"Never Leave You (Uh Oooh, Uh Oooh)":   118,
+"Paper Planes":   77,
+"Hung Up":   156,
+"We Belong Together":   173,
+"Let Me Love You":   38,
+"She Will Be Loved":   44,
+"Family Affair":   114,
+"Kids":   170,
+"Haven't Met You Yet":   61,
+"Grace Kelly":   78,
+"Party In The USA":   193,
+"This Is Why I'm Hot":   190,
+"Work It":   51,
+"Lady - Hear Me Tonight":   142,
+"Uprising":   25,
+"Welcome To The Black Parade":   3,
+"Unwritten":   45,
+"So Sick":   131,
+"Hot In Here":   121,
+"I'm Like A Bird":   90,
+"How You Remind Me":   46,
+"Don't Know Why":   194,
+"Bye Bye Bye":   62,
+"Dragostea Din Tei":   19,
+"Ice Box":   149,
+"The Magic Key":   93,
+"Stop And Stare":   100,
+"Hey Ya!":   36,
+"Fireflies":   58,
+"I Write Sins Not Tradegies":   89,
+"Last Resort":   15,
+"U + Ur Hand":   33,
+"Hey There Delilah":   8,
+"Blurry":   35,
+"Sonne":   17,
+"Dani California":   14,
+"She Bangs":   144,
+"Umbrella":   16,
+"Lonely No More":   85,
+"Life Is A Rollingcoaster":   29,
+"Feel":   139,
+"By Your Side":   200,
+"The Bongo Song":   97,
+"Love Song":   111,
+"Beautiful Girls":   158,
+"Get Busy":   65,
+"It Wasn't Me":   54,
+"Whenever, Wherever":   42,
+"Drop It Like It's Hot":   12,
+"Murder On The Dancefloor":   10,
+"Crank That (Soulja Boy)":   124,
+"There's Gotta Be More To Life":  163,
+"Its Been Awhile":   32,
+"Push The Button":   191,
+"Still Waiting":   41,
+"Chop Suey!":   2,
+"All The Things She Said":   67,
+"Whatever You Like":   195,
+"Break Your Heart":   150,
+"Love Story":   143,
+"Lean Back":   138,
+"Gives You Hell":   53,
+"Wherever You Will Go":   47,
+"How To Save A Life":   165,
+"In The Shadows":   69,
+"Face Down":   20,
+"Human":   91,
+"Beep":   164,
+"The Man Who Can't Be Moved":   30,
+"Untouched":   120,
+"Seven Nations Army":   123,
+"The Way I Are":   157,
+"He Wasn't Man Enough":   181,
+"Hey Soul Sister":   125,
+"Headstrong":   4,
+"Sing":   68,
+"Beautiful Day":   129,
+"Yeah!":   92,
+"A Thousand Miles":   9,
+"Island In The Sun":   155,
+"When You're Looking Like That":   87,
+"Teenage Dirtbag":   147}
+export default kukiScore;
